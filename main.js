6d061674a6386cdc55054b1334c239b6833c4c31
gabecosta <127759934+gabecosta@users.noreply.github.com>
2024-03-27 13:49:23 -0300

    main.js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo")
 for(let i=0; i<botoes.length; i++){
 botoes[i].onclick = function(){
+    textos[j].classList.remove("ativo")
 for(let j=0; j<botoes.length; j++){
+    textos[j].classList.remove("ativo")
 botoes[j].classList.remove("ativo");
 }
 botoes[i].classList.add("ativo");
